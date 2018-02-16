04a4968b455035d7623924362110faed9a972b90
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 02:45:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,6 @@ def position_taken?(board, index)
   if board[index] == " " || board[index].to_s.empty?
     return false
   elsif board[index].eql? "X" || board[index].eql? "O"
-    return true
+  return true
   end
 end
